3ea6c34862003ef9a97e8713c53d87d6e0bddd3a
MiracKoksal1 <mirac.koksal09@gmail.com>
2024-01-05 19:46:30 +0300

    first data commited

---
+++ src/pages/pages/authentication/login-v1.vue
@@ -0,0 +1,190 @@
+<script setup lang="ts">
+import authV1BottomShape from '@images/svg/auth-v1-bottom-shape.svg?raw'
+import authV1TopShape from '@images/svg/auth-v1-top-shape.svg?raw'
+import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
+import { themeConfig } from '@themeConfig'
+import { VForm } from 'vuetify/components/VForm'
+
+
+
+
+definePage({
+  meta: {
+    layout: 'blank',
+  },
+})
+
+
+
+const credentials = ref({
+  email: '', //'admin@demo.com',
+  password: '', // 'admin',
+  rememberMe: true,
+})
+const isPasswordVisible = ref(false)
+
+const route = useRoute()
+const router = useRouter()
+
+const ability = useAbility()
+
+const errors = ref<Record<string, string | undefined>>({
+  email: undefined,
+  password: undefined,
+})
+
+const refVForm = ref<VForm>()
+
+const rememberMe = ref(false)
+
+const login = async () => {
+  try {
+    const res = await $api('/auth/login', {
+      method: 'POST',
+      body: {
+        email: credentials.value.email,
+        password: credentials.value.password,
+      },
+      onResponseError({ response }) {
+        errors.value = response._data.errors
+      },
+    })
+
+    const { accessToken, userData, userAbilityRules } = res
+
+    useCookie('userAbilityRules').value = userAbilityRules
+    ability.update(userAbilityRules)
+
+    useCookie('userData').value = userData
+    useCookie('accessToken').value = accessToken
+
+    // Redirect to `to` query if exist or redirect to index route
+    // ❗ nextTick is required to wait for DOM updates and later redirect
+    await nextTick(() => {
+      router.replace(route.query.to ? String(route.query.to) : '/')
+    })
+  }
+  catch (err) {
+    console.error(err)
+  }
+}
+
+const onSubmit = () => {
+  refVForm.value?.validate()
+    .then(({ valid: isValid }) => {
+      if (isValid)
+        login()
+    })
+}
+</script>
+
+<template>
+  <div class="auth-wrapper d-flex align-center justify-center pa-4">
+    <div class="position-relative my-sm-16">
+      <!-- 👉 Top shape -->
+      <VNodeRenderer
+        :nodes="h('div', { innerHTML: authV1TopShape })"
+        class="text-primary auth-v1-top-shape d-none d-sm-block"
+      />
+
+      <!-- 👉 Bottom shape -->
+      <VNodeRenderer
+        :nodes="h('div', { innerHTML: authV1BottomShape })"
+        class="text-primary auth-v1-bottom-shape d-none d-sm-block"
+      />
+
+      <!-- 👉 Auth Card -->
+      <VCard
+        class="auth-card pa-4"
+        max-width="448"
+      >
+
+        <VCardItem class="justify-center">
+          <template #prepend>
+            <div class="d-flex">
+              <RouterLink
+        to="/"
+        class="app-logo d-flex align-center gap-x-3"
+      >
+        <VNodeRenderer :nodes="themeConfig.app.logo" />
+      </RouterLink>
+            </div>
+          </template>
+
+        </VCardItem>
+
+        <VCardText>
+          <VForm @submit.prevent="() => {}">
+            <VRow>
+              <!-- email -->
+              <VCol cols="12">
+                <AppTextField
+                  v-model="credentials.email"
+                  autofocus
+                  label="Email"
+                  type="email"
+                  :rules="[requiredValidator, emailValidator]"
+                  placeholder="johndoe@email.com"
+                />
+              </VCol>
+
+              <!-- password -->
+              <VCol cols="12">
+                <AppTextField
+                  v-model="credentials.password"
+                  label="Password"
+                  placeholder="············"
+                  :type="isPasswordVisible ? 'text' : 'password'"
+                  :rules="[requiredValidator]"
+                  :append-inner-icon="isPasswordVisible ? 'tabler-eye-off' : 'tabler-eye'"
+                  @click:append-inner="isPasswordVisible = !isPasswordVisible"
+                />
+
+                <!-- remember me checkbox -->
+                <div class="d-flex align-center justify-space-between flex-wrap mt-2 mb-4">
+                  <VCheckbox
+                    v-model="rememberMe"
+                    label="Beni Hatırla"
+                  />
+
+                  <RouterLink
+                    class="text-primary ms-2 mb-1"
+                    :to="{ name: 'pages-authentication-forgot-password-v1' }"
+                  >
+                    Şifremi Unuttum
+                  </RouterLink>
+                </div>
+
+                <!-- login button -->
+                <VBtn
+                  block
+                  type="submit"
+                >
+                  Giriş
+                </VBtn>
+              </VCol>
+
+              <!-- create account -->
+              <VCol
+                cols="12"
+                class="text-center text-base"
+              >
+                <span>Yeni kullanıcı mısın?</span>
+                <RouterLink
+                  class="text-primary ms-2"
+                  :to="{ name: 'pages-authentication-register-v1' }"
+                >
+                  Hesap Oluştur
+                </RouterLink>
+              </VCol>
+            </VRow>
+          </VForm>
+        </VCardText>
+      </VCard>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use "@core/scss/template/pages/page-auth.scss";
+</style>
